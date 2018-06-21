d384f7de09f01f6bca4a7f417a1ef6d3674efa08
Hana Stauss <hanastauss@icloud.com>
2018-06-21 21:43:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file.
+
+const newArray = []
+const newArray2 = []
+const newArray3 = [] 
+
+function change(thing) {
+  thing = thing.toLowerCase()
+  newArray.push(thing)
+}
+
+function lowerCaseStudentNames(array) {
+  array.map(change)
+  return newArray
+}
+
+function alter(thing2) {
+  thing2 = thing2.split(" ")
+  thing2 = {firstName: thing2[0], lastName: thing2[1]}
+  newArray2.push(thing2)
+}
+
+function nameToAttributes(array) {
+  array.map(alter)
+  return newArray2
+}
+
+function modify(thing3) {
+  newArray3.push(`${thing3.name} is from ${thing3.hometown}`)
+}
+
+function attributesToPhrase(array) {
+  array.map(modify)
+  return newArray3
+} 
